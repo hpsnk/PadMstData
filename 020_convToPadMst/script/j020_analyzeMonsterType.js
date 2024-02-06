69dcefe537906aca76a73af90932206faaf26d13
happysonic <archer03371@163.com>
2024-02-06 14:06:17 +0800

    add customer type csv

--- 020_convToPadMst/script/j020_analyzeMonsterType.js
+++ 020_convToPadMst/script/j020_analyzeMonsterType.js
@@ -31,9 +31,35 @@ let arrayUniqueTypeId = Array.from(new Set(arrayTpyeId.flat(Infinity))).filter((
 });
 // console.log(arrayUniqueTypeId);
 
-let arrayType = arrayUniqueTypeId.map(valTypeId => {
-  return {typeId : valTypeId}
+// ----------------------------
+// 读取 custom/type.csv
+const strCustomType = fs.readFileSync(
+  "custom/type.csv",
+  "utf-8",
+);
+let customTypeMap = [];
+strCustomType.split('\r\n').forEach(element => {
+  let infos = element.split(',');
+  let id    = parseInt(infos[0]);
+  let name  = infos[1];
+  let order = parseInt(infos[2]);
+
+  customTypeMap[id] = {
+    typeId: id,
+    name:   name,
+    order:  order,
+  };
 });
+
+let arrayType = arrayUniqueTypeId.map(valTypeId => {
+  if (customTypeMap[valTypeId] != undefined) {
+    return customTypeMap[valTypeId];
+  } else {
+    return {typeId : valTypeId}
+  }
+}).sort(function(a,b){
+  return a.order-b.order;
+});;
 // console.log(arrayType);
 console.log("  MonsterType Size = %d.", arrayType.length);
 
