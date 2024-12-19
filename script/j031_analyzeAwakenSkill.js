1b011386d2f3d4c04e41eaea6fc32e7baf257aac
zhanghao <archer03371@163.com>
2024-12-20 00:15:10 +0800

    添加注释

--- script/j031_analyzeAwakenSkill.js
+++ script/j031_analyzeAwakenSkill.js
@@ -11,6 +11,7 @@ if (!fs.existsSync("../json/monster.json")) {
 
 console.log("analyzeAwakenSkill-->start.");
 
+// 读取monster信息
 const strMonster = fs.readFileSync(
   "../json/monster.json",
   "utf-8",
@@ -19,17 +20,22 @@ const strMonster = fs.readFileSync(
 const jsonMonster = JSON.parse(strMonster);
 console.log("  PadMst Monster Size =%d.", jsonMonster.length);
 
+// 提取所有的awakenskillid
 let arrayAwakenSkill = jsonMonster.map((monster, idx) => {
   return monster.awakenskillIds;
 });
 
+// awakenskillid 去重，排序
 let arrayUniqueAwakenSkill = Array.from(
-  new Set(arrayAwakenSkill.flat(Infinity))
+  new Set(                               // 去除重复项
+    arrayAwakenSkill.flat(Infinity)      // 数组扁平化(变成一维数组)
+  )
 ).sort(function(a,b){
   return a-b;
 });
 // console.log(arrayUniqueAwakenSkill);
 
+// 写入txt文件
 fs.writeFileSync("../json/awakenskill.txt", JSON.stringify(arrayUniqueAwakenSkill));
 
 console.log("analyzeAwakenSkill-->end.");
